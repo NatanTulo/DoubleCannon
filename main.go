a12f08e1fcca9a59c176fca70c6780f2b64fbaaf
NatanTulo <135972515+NatanTulo@users.noreply.github.com>
2024-11-17 21:59:26 +0100

    prediction landing circle

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"math/rand"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -14,7 +15,6 @@ func main() {
 	defer rl.CloseWindow()
 
 	rl.SetConfigFlags(rl.FlagVsyncHint)
-	// rl.SetConfigFlags(rl.FlagBorderlessWindowedMode)
 	camera := rl.Camera3D{
 		Position:   rl.NewVector3(4, 2, 4),
 		Target:     rl.NewVector3(0, 1.8, 0),
@@ -23,11 +23,6 @@ func main() {
 		Projection: rl.CameraProjection(rl.CameraPerspective),
 	}
 
-	initialTarget := camera.Target
-	initialDirection := rl.Vector3Normalize(rl.Vector3Subtract(camera.Target, camera.Position))
-	spherePosition := initialTarget
-
-	// rl.ToggleFullscreen()
 	heights := make([]float32, 20)
 	positions := make([]rl.Vector3, 20)
 	colors := make([]rl.Color, 20)
@@ -42,12 +37,43 @@ func main() {
 	var time float32
 	var isProjectileActive bool
 	var wasMousePressed bool = false
-	var initialSpeed float32
 	var gravity float32 = 9.81
 	var mousePressTime float32
 	var isMousePressed bool
 	const MAX_SPEED float32 = 50.0
 	const SPEED_SCALER float32 = 20.0
+	var landingPoint rl.Vector3
+	var spherePosition rl.Vector3
+
+	var projectileStartPos rl.Vector3
+	var projectileDirection rl.Vector3
+	var projectileSpeed float32
+
+	// Funkcja obliczająca czas lotu
+	calculateTimeOfFlight := func(v0y float32, y0 float32) float32 {
+		a := gravity / 2
+		b := -v0y
+		c := -y0
+
+		return float32((-float64(b) + math.Sqrt(float64(b*b-4*a*c))) / (2 * float64(a)))
+	}
+
+	// Funkcja obliczająca pozycję w danym czasie
+	calculatePosition := func(startPos rl.Vector3, direction rl.Vector3, speed float32, t float32) rl.Vector3 {
+		return rl.NewVector3(
+			startPos.X+speed*direction.X*t,
+			startPos.Y+speed*direction.Y*t-0.5*gravity*t*t,
+			startPos.Z+speed*direction.Z*t,
+		)
+	}
+
+	calculateLandingPoint := func(direction rl.Vector3, speed float32, startPos rl.Vector3) rl.Vector3 {
+		v0y := speed * direction.Y
+		timeOfFlight := calculateTimeOfFlight(v0y, startPos.Y)
+		position := calculatePosition(startPos, direction, speed, timeOfFlight)
+		position.Y = 0 // Upewniamy się, że punkt lądowania jest dokładnie na poziomie ziemi
+		return position
+	}
 
 	for !rl.WindowShouldClose() {
 		rl.UpdateCamera(&camera, rl.CameraFirstPerson)
@@ -68,27 +94,28 @@ func main() {
 		if rl.IsMouseButtonReleased(rl.MouseLeftButton) {
 			isMousePressed = false
 			wasMousePressed = true
-			initialSpeed = mousePressTime * SPEED_SCALER
-			if initialSpeed > MAX_SPEED {
-				initialSpeed = MAX_SPEED
-			}
 			time = 0.0
-			initialTarget = camera.Target
-			initialDirection = rl.Vector3Normalize(rl.Vector3Subtract(camera.Target, camera.Position))
-			spherePosition = initialTarget
+
+			projectileStartPos = camera.Position
+			projectileDirection = rl.Vector3Normalize(rl.Vector3Subtract(camera.Target, camera.Position))
+			projectileSpeed = mousePressTime * SPEED_SCALER
+			if projectileSpeed > MAX_SPEED {
+				projectileSpeed = MAX_SPEED
+			}
+
+			spherePosition = projectileStartPos
 			isProjectileActive = true
 		}
-		// Obliczanie pozycji kuli zgodnie z trajektorią
-		if isProjectileActive {
-			spherePosition = rl.NewVector3(
-				initialTarget.X+initialSpeed*initialDirection.X*time,
-				initialTarget.Y+initialSpeed*initialDirection.Y*time-0.5*gravity*time*time,
-				initialTarget.Z+initialSpeed*initialDirection.Z*time,
-			)
 
-			// Zatrzymanie ruchu, jeśli kula spadnie poniżej płaszczyzny
+		if isProjectileActive {
+			spherePosition = calculatePosition(projectileStartPos, projectileDirection, projectileSpeed, time)
+
 			if spherePosition.Y <= 0 {
 				isProjectileActive = false
+				// Obliczamy dokładną pozycję lądowania
+				v0y := projectileSpeed * projectileDirection.Y
+				timeOfFlight := calculateTimeOfFlight(v0y, projectileStartPos.Y)
+				spherePosition = calculatePosition(projectileStartPos, projectileDirection, projectileSpeed, timeOfFlight)
 				spherePosition.Y = 0
 			}
 		}
@@ -99,8 +126,20 @@ func main() {
 		rl.BeginMode3D(camera)
 
 		if wasMousePressed {
-			rl.DrawSphere(spherePosition, 0.1, rl.Red)
+			rl.DrawSphere(spherePosition, 0.2, rl.Red)
+			rl.DrawSphere(landingPoint, 0.1, rl.Blue)
+			rl.DrawCircle3D(landingPoint, 1.0, rl.NewVector3(1, 0, 0), 90, rl.Fade(rl.Blue, 0.5))
 		}
+
+		if isMousePressed {
+			currentDirection := rl.Vector3Normalize(rl.Vector3Subtract(camera.Target, camera.Position))
+			initialSpeed := mousePressTime * SPEED_SCALER
+			if initialSpeed > MAX_SPEED {
+				initialSpeed = MAX_SPEED
+			}
+			landingPoint = calculateLandingPoint(currentDirection, initialSpeed, camera.Position)
+		}
+
 		rl.DrawPlane(rl.NewVector3(0, 0, 0), rl.NewVector2(128, 128), rl.LightGray)
 
 		for i := 0; i < 20; i++ {
