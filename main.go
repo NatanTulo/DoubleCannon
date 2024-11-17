fb1c0270a2a701402f565cb7743eb1e335d743e2
NatanTulo <135972515+NatanTulo@users.noreply.github.com>
2024-11-17 15:20:46 +0100

    One ball physics

--- main.go
+++ main.go
@@ -39,31 +39,68 @@ func main() {
 	}
 
 	rl.SetTargetFPS(120)
-	var distance float32
-	speed := 0.1
+	var time float32
+	var isProjectileActive bool
+	var wasMousePressed bool = false
+	var initialSpeed float32
+	var gravity float32 = 9.81
+	var mousePressTime float32
+	var isMousePressed bool
+	const MAX_SPEED float32 = 50.0
+	const SPEED_SCALER float32 = 20.0
 
 	for !rl.WindowShouldClose() {
 		rl.UpdateCamera(&camera, rl.CameraFirstPerson)
+		time += rl.GetFrameTime()
 
 		rl.SetMousePosition(int(screenWidth)/2, int(screenHeight)/2)
 		rl.DisableCursor()
+
+		if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+			mousePressTime = 0.0
+			isMousePressed = true
+		}
+
+		if isMousePressed {
+			mousePressTime += rl.GetFrameTime()
+		}
+
+		if rl.IsMouseButtonReleased(rl.MouseLeftButton) {
+			isMousePressed = false
+			wasMousePressed = true
+			initialSpeed = mousePressTime * SPEED_SCALER
+			if initialSpeed > MAX_SPEED {
+				initialSpeed = MAX_SPEED
+			}
+			time = 0.0
+			initialTarget = camera.Target
+			initialDirection = rl.Vector3Normalize(rl.Vector3Subtract(camera.Target, camera.Position))
+			spherePosition = initialTarget
+			isProjectileActive = true
+		}
+		// Obliczanie pozycji kuli zgodnie z trajektorią
+		if isProjectileActive {
+			spherePosition = rl.NewVector3(
+				initialTarget.X+initialSpeed*initialDirection.X*time,
+				initialTarget.Y+initialSpeed*initialDirection.Y*time-0.5*gravity*time*time,
+				initialTarget.Z+initialSpeed*initialDirection.Z*time,
+			)
+
+			// Zatrzymanie ruchu, jeśli kula spadnie poniżej płaszczyzny
+			if spherePosition.Y <= 0 {
+				isProjectileActive = false
+				spherePosition.Y = 0
+			}
+		}
 
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.SkyBlue)
 
 		rl.BeginMode3D(camera)
 
-		if rl.IsMouseButtonDown(rl.MouseLeftButton) {
-			distance = 0.0
-			initialTarget = camera.Target
-			initialDirection = rl.Vector3Normalize(rl.Vector3Subtract(camera.Target, camera.Position))
-			spherePosition = initialTarget
+		if wasMousePressed {
+			rl.DrawSphere(spherePosition, 0.1, rl.Red)
 		}
-
-		distance += float32(speed)
-		spherePosition = rl.Vector3Add(initialTarget, rl.Vector3Scale(initialDirection, distance))
-
-		rl.DrawSphere(spherePosition, 0.1, rl.Red)
 		rl.DrawPlane(rl.NewVector3(0, 0, 0), rl.NewVector2(128, 128), rl.LightGray)
 
 		for i := 0; i < 20; i++ {
@@ -74,13 +111,15 @@ func main() {
 		rl.EndMode3D()
 
 		rl.DrawCircle(screenWidth/2, screenHeight/2, 3, rl.Black)
-		// rl.DrawRectangle(10, 10, 220, 70, rl.Fade(rl.SkyBlue, 0.5))
-		// rl.DrawRectangleLines(10, 10, 220, 70, rl.Blue)
-		rl.DrawFPS(20, 20)
 
-		// rl.DrawText("First person camera default controls:", 20, 20, 10, rl.Black)
-		// rl.DrawText("- Move with keys: W, A, S, D", 40, 40, 10, rl.DarkGray)
-		// rl.DrawText("- Mouse move to look around", 40, 60, 10, rl.DarkGray)
+		if mousePressTime*SPEED_SCALER < MAX_SPEED {
+			rl.DrawRectangle(10, 50, int32(220.0*mousePressTime*SPEED_SCALER/MAX_SPEED), 50, rl.Fade(rl.Black, 0.5))
+		} else {
+			rl.DrawRectangle(10, 50, 220, 50, rl.Fade(rl.Red, 0.5))
+		}
+		rl.DrawRectangleLines(10, 50, 220, 50, rl.Blue)
+
+		rl.DrawFPS(20, 20)
 
 		rl.EndDrawing()
 	}
